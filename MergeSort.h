4e8fc379d510bd463f5f991c32dd5590bf43fa1e
henrisama <pedropiresflores@icloud.com>
2020-03-18 16:12:01 -0400

    update

--- MergeSort.h
+++ MergeSort.h
@@ -1,12 +1,12 @@
-void Merge(int p, int q, int r, int *array){
+void Merge(int begin, int middle, int end, int *array){
 	int i, j, k, *aux;
-	aux = (int*) malloc((r-p) * sizeof(int));
+	aux = (int*) malloc((end-begin) * sizeof(int));
 
-	i = p;
-	j = q;
+	i = begin;
+	j = middle;
 	k = 0;
 
-	while(i < q && j < r){
+	while(i < middle && j < end){
 		if(array[i] < array[j]){
 			aux[k] = array[i];
 			i++;
@@ -17,25 +17,24 @@ void Merge(int p, int q, int r, int *array){
 		}
 		k++;
 	}
-	while(i < q){
+	while(i < middle){
 		aux[k] = array[i];
 		i++; k++;
 	}
-	while(j < r){
+	while(j < end){
 		aux[k] = array[j];
 		j++; k++;
 	}
-	for(i=p; i<r; i++)
-		array[i] = aux[i-p];
+	for(i=begin; i<end; i++)
+		array[i] = aux[i-begin];
 	free(aux);
 }
 
-void MergeSort(int p, int r, int *array){
-	int q;
-	if(p < r-1){
-		q = (p+r)/2;
-		MergeSort(p, q, array);
-		MergeSort(q, r, array);
-		Merge(p, q, r, array);
+void MergeSort(int begin, int end, int *array){
+	if(begin < end-1){
+		int middle = (begin+end)/2;
+		MergeSort(begin, middle, array);
+		MergeSort(middle, end, array);
+		Merge(begin, middle, end, array);
 	}
-}
+}//complexity O(nlogn)
